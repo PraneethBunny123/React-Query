4c712ad569452367a1d1b59cdcffa00759f15aa8
praneeth <gp.gunturu@gmail.com>
2025-05-23 16:23:33 -0500

    getting values from the useQuery hook and using them to handle loading and error states

--- src/components/Events/FindEventSection.jsx
+++ src/components/Events/FindEventSection.jsx
@@ -1,12 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { useRef, useState } from 'react';
 import { fetchEvents } from '../../util/http';
+import LoadingIndicator from '../UI/LoadingIndicator';
+import ErrorBlock from '../UI/ErrorBlock';
 
 export default function FindEventSection() {
   const [searchTerm, setSearchTerm] = useState('')
   const searchElement = useRef();
 
-  useQuery({
+  const {data, isPending, isError, error} = useQuery({
     queryKey: ['events', {search: searchTerm}],
     queryFn: () => fetchEvents(searchTerm)
   })
@@ -14,6 +16,20 @@ export default function FindEventSection() {
   function handleSubmit(event) {
     event.preventDefault();
     setSearchTerm(searchElement)
+  }
+
+  let content = <p>Please enter a search term and to find events.</p>
+
+  if(isPending) {
+    content = <LoadingIndicator />
+  }
+
+  if(isError) {
+    content = <ErrorBlock title='An error occured' message={error.info?.message || 'Failed to fetch event'} />
+  }
+
+  if(data) {
+    
   }
 
   return (
@@ -29,7 +45,6 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
     </section>
   );
 }
