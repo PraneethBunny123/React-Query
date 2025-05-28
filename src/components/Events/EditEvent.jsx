46343b8a746b3886fc33a84bc78745888a54d195
praneeth <gp.gunturu@gmail.com>
2025-05-28 17:40:47 -0500

    using onMutate function inside the useMutation instead of onSuccess to get the optimised update. This function will be triggered as soon as the mutate function is executed

--- src/components/Events/EditEvent.jsx
+++ src/components/Events/EditEvent.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { fetchEvent, updateEvent } from '../../util/http.js';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx'
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -19,6 +19,9 @@ export default function EditEvent() {
 
   const {mutate} = useMutation({
     mutationFn: updateEvent,
+    onMutate: () => {
+      queryClient
+    }
   })
 
   function handleSubmit(formData) {
